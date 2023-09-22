3185ead9cbc9a48b23d9aa159480a24946c6c6f7
nishiiut <74111121+nishiiut@users.noreply.github.com>
2023-09-22 12:11:29 -0400

    Add Web App

---
+++ ICA3_WebApp.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv('data 2.csv')
+
+st.write("""
+# WI Cancer dataset
+Wisconsin Breast Cancer Diagnostic dataset
+""")
+
+x_axis = st.selectbox(
+    'choose a column for the x-axis to plot',
+    ('radius_mean', 'texture_mean', 'perimeter_mean',
+       'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
+       'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+       'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+       'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+       'fractal_dimension_se', 'radius_worst', 'texture_worst',
+       'perimeter_worst', 'area_worst', 'smoothness_worst',
+       'compactness_worst', 'concavity_worst', 'concave points_worst',
+       'symmetry_worst', 'fractal_dimension_worst')
+)
+
+y_axis = st.selectbox(
+    'choose a column for the y-axis to plot',
+    ('radius_mean', 'texture_mean', 'perimeter_mean',
+       'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
+       'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+       'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+       'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+       'fractal_dimension_se', 'radius_worst', 'texture_worst',
+       'perimeter_worst', 'area_worst', 'smoothness_worst',
+       'compactness_worst', 'concavity_worst', 'concave points_worst',
+       'symmetry_worst', 'fractal_dimension_worst')
+)
+
+
+fig1 = sns.lmplot(data=df, x=x_axis, y=y_axis, hue='diagnosis')
+
+st.pyplot(fig1)
