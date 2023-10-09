22563ad625822bd8d41e0225efada4a5143e4327
Uta Nishii <nishiiut@msu.edu>
2023-10-09 08:01:04 -0400

    adding file

--- hint5.py
+++ hint5.py
@@ -1,6 +1,34 @@
-# my_streamlit_app.py
-
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+@st.cache_data
+def load_data():
+    return pd.read_sas('hints5_cycle4_public.sas7bdat')
 
-st.markdown("Hi")
+df = load_data()
+
+st.title("Hints5 Cycle4")
+
+plot_choice = st.sidebar.selectbox(
+    "Choose a Plot Type:",
+    ["Line Plot of DRA", "Histogram of Weekly Minutes of Moderate Exercise"]
+)
+
+if plot_choice == "Line Plot of DRA":
+    st.write("Displaying Line Plot of DRA")
+    fig, ax = plt.subplots()
+    ax.plot(df['DRA'])
+    ax.set_title('Line Plot of DRA')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('DRA')
+    st.pyplot(fig)
+
+elif plot_choice == "Histogram of Weekly Minutes of Moderate Exercise":
+    st.write("Displaying Histogram of Weekly Minutes of Moderate Exercise")
+    fig, ax = plt.subplots()
+    ax.hist(df['WeeklyMinutesModerateExercise'].dropna(), bins=30) 
+    ax.set_title('Histogram of Weekly Minutes of Moderate Exercise')
+    ax.set_xlabel('Weekly Minutes of Moderate Exercise')
+    ax.set_ylabel('Frequency')
+    st.pyplot(fig)
